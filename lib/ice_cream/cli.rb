17d8ce59f07b41f245cfb4a6a2e041e382bbefe3
alicia <afasciocco@gmail.com>
2019-08-02 13:41:47 -0400

    added cli code to choose_shop

--- lib/ice_cream/cli.rb
+++ lib/ice_cream/cli.rb
@@ -1,6 +1,35 @@
 class IceCream::CLI
 
     def call
-        puts "Are you in the mood for ice cream?"
+        self.welcome
+        main_menu
+        loop do
+            user_input = main_menu
+            if user_input == 'exit' || user_input.include?('n')
+              return
+            else
+              self.list_shops
+              self.choose_shop
+            end
+          end
+  
     end
-end
+
+    def welcome
+        puts "Welcome! Are you in the mood for ice cream?"
+        input = gets.strip.downcase
+        return input
+    end
+        
+    def list_shops 
+        puts "shops 1-10"
+    end
+
+    def choose_shop
+        puts "Choose an ice cream shop to read more about it."
+          index = gets.strip.to_i - 1
+          movie = Movie.all[index]
+          Scraper.scrape_info(movie)
+          self.display_movie_info(movie)
+      end
+    end
