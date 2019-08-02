2d4a998fd6810f764fcd8673256e6a443e9d4fe0
alicia <afasciocco@gmail.com>
2019-08-02 11:04:03 -0400

    created Scraper class

---
+++ lib/cli.rb
@@ -0,0 +1,5 @@
+class CLI
+    def run
+        puts "CLI is running."
+    end
+end
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,8 @@
-require "ice_cream/version"
+require_relative "./ice_cream/version"
 
 module IceCream
   class Error < StandardError; end
   # Your code goes here...
 end
+
+require_relative "./cli"
