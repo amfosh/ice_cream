b1c27c045606d181cae839d09b92b19f57bd4e22
alicia <afasciocco@gmail.com>
2019-08-03 14:20:23 -0400

    scraping shops

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ class Scraper
 
     def self.scrape_shops
       doc = Nokogiri::HTML(open("https://www.theinfatuation.com/seattle/guides/ice-cream-seattle-power-rankings"))
-      shop_list = doc.search("spot-block.a h3").text.strip
+      shop_list = doc.search("h3").text.strip
       shop_list
       binding.pry
     end
