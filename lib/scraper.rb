9167530cab4c9723ae57d49dce1b21772e24166a
alicia <afasciocco@gmail.com>
2019-08-03 14:17:05 -0400

    more tests passing - added cli info per lantz

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ class Scraper
 
     def self.scrape_shops
       doc = Nokogiri::HTML(open("https://www.theinfatuation.com/seattle/guides/ice-cream-seattle-power-rankings"))
-      shop_list = doc.css("h3.spot-block").text.strip
+      shop_list = doc.search("spot-block.a h3").text.strip
       shop_list
       binding.pry
     end
