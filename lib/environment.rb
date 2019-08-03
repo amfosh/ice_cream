ee1ee06fe5d56f67bcfe015f5296bcbbc5342b62
alicia <afasciocco@gmail.com>
2019-08-03 09:15:22 -0400

    lots of errors happening

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,6 +1,9 @@
+require 'open-URI'
+require 'nokogiri'
 require_relative "./version"
 require_relative "./cli"
 require_relative "./shop"
+require_relative "./scraper"
 
 # module IceCream
 #   class Error < StandardError; end
