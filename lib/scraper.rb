0bd4800b364940a29f5058c1e549302d66fb89f1
alicia <afasciocco@gmail.com>
2019-08-04 14:25:20 -0400

    closer to array

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,20 +3,29 @@ require "pry"
 class Scraper
 
     # def self.scrape
-      @@SHOPS = []
       # shops << self.scrape_shops
     # end
 
     # BASE_URL = "https://www.theinfatuation.com/seattle/guides/ice-cream-seattle-power-rankings"
 
     def self.scrape_shops
+      shop_names = []
       doc = Nokogiri::HTML(open("https://www.theinfatuation.com/seattle/guides/ice-cream-seattle-power-rankings"))
-      shop_list = doc.search("spot-block__title-copy.h3")
-      # .each do |shop_doc|e
-      @@SHOPS << shop_list
-      @@SHOPS
-      # shop_list.each do |shop_doc|
+      shop_list = doc.search(".spot-block__title-copy h3").map{|h3|
+    h3.text
+      }
+      shop_names << shop_list
       binding.pry
+
+      # each do |shop_doc|
+
+      #   shop_name = shop_doc.css("h3").
+      # shop_names
+      # .each do |shop_doc|e
+      # @@SHOPS << shop_list
+      # @@SHOPS
+      # shop_list.each do |shop_doc|
+    
     end
 
     def self.scrape_review(shop)
