a00d14cbe116e39fdcb2935ff978e8d92535753a
alicia <afasciocco@gmail.com>
2019-08-06 18:00:00 -0400

    refactoring - dupe again

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,24 +2,27 @@ require "pry"
 
 class Scraper
 
-    # def self.scrape
-      # shops << self.scrape_shops
-    # end
-
-    # BASE_URL = "https://www.theinfatuation.com/seattle/guides/ice-cream-seattle-power-rankings"
-
     def self.scrape_shops
-      html = open("https://www.theinfatuation.com/seattle/guides/ice-cream-seattle-power-rankings")
-      doc = Nokogiri::HTML(html)
-      shop_list = doc.search(".spot-block__title-copy h3").map{|h3| h3.text.strip}
-      shop_list.uniq.each.with_index(1) do |shop, index| 
-        puts "#{index}. #{shop}"
+      doc = Nokogiri::HTML(open("https://www.theinfatuation.com/seattle/guides/ice-cream-seattle-power-rankings"))
+      doc.search(".spot-block__title-copy").each do |shop_doc|
+        name = shop_doc.search("h3").text.strip
+
+        Shop.new(name)
+        Shop.all.uniq.each.with_index(1) do |shop, index| 
+          puts "#{index}. #{shop.name}"
+      # map{|h3| h3.text.strip}
+      # url = 
+      
+      # shop_list.uniq.each.with_index(1) do |shop, index| 
+      #   puts "#{index}. #{shop}"
       # shop_list.each do |div|
       #   input_arg = {
       #     url: doc.search('.spot-block__title-copy h3 a').attr('href')
       # }
       # binding.pry
+      end
     end
+  end
 
     def self.scrape_review(shop)
       puts input_arg
@@ -30,5 +33,4 @@ class Scraper
       html.css("a.review")[2].text.split(',').map(&:strip)
       shop.review = ("p")
     end
-end
-end
+  end
