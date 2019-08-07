8ad6b8af7e10e0bc85b18d2f8fe45f41ae8361a1
alicia <afasciocco@gmail.com>
2019-08-07 18:43:39 -0400

    back to working

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,51 +1,73 @@
 class CLI
 
     def run
-        self.welcome
-        loop do
-            user_input = main_menu
-            if user_input == 'exit' || user_input.include?('n')
-              return
-            else
-              # self.choose_shop
-              self.list_shops
+      shop_names = []
+      puts "Welcome ice cream lover!"
 
-            end
-          end
-        end 
+      # html = open("https://www.theinfatuation.com/seattle/guides/ice-cream-seattle-power-rankings")
+      doc = Nokogiri::HTML(open("https://www.theinfatuation.com/seattle/guides/ice-cream-seattle-power-rankings"))
+      name = doc.css(".spot-block__title-copy h3").map{|h3| h3.text.strip}
+      shop_name = name.uniq
 
+      shop_name.each.with_index(1) do |shop_name, index| 
+        puts "#{index}. #{shop_name}"
 
-    def welcome
-        puts "Welcome ice cream lover!"
-    end
+      # binding.pry
+     
+      # shop_name = name.uniq
 
-    def main_menu
-      puts "Are you in the mood for ice cream?"
-      input = gets.strip.downcase
-      return input
-    end
+        # shop_names << name
 
-    def list_shops 
-      puts "Choose an ice cream shop to read more about it."
-      Scraper.scrape_shops
-      input = gets.chomp
-      shop = Shop.all[input.to_i - 1]
-      Shop.all.each.with_index(1) do |name, index| 
-        puts "#{index}. #{shop.name}"
-    end
-
-    # def choose_shop
-    #   puts "Choose an ice cream shop to read more about it."
-
-    #     Scraper.scrape_review(shop)
-      # list_shops
-      #   index = gets.strip.to_i - 1
-      #   shop = Shop.all[index]
-      #   Scraper.scrape_shops[index]
-      #   # self.scrape_shops
+        # shop_names.each.with_index(1) do |name, i|
+        #   puts "#{i}. #{name}"
+      # 
       end
-
-    def display_shop_info(shop)
- #
     end
   end
+#         self.welcome
+#         loop do
+#             user_input = main_menu
+#             if user_input == 'exit' || user_input.include?('n')
+#               return
+#             else
+#               self.list_shops
+#               self.choose_shop
+#             end
+#           end
+#         end 
+
+
+#     def welcome
+#         
+#     end
+
+#     def main_menu
+#       puts "Are you in the mood for ice cream?"
+#       input = gets.strip.downcase
+#       return input
+#     end
+
+#     def list_shops 
+#       Scraper.scrape_shops
+#     end
+
+#     def choose_shop
+#       puts "Choose an ice cream shop to read more about it."
+#       Scraper.scrape_shops
+  
+#         input = gets.chomp
+
+
+
+#         Scraper.scrape_review(shop)
+#       # list_shops
+#       #   index = gets.strip.to_i - 1
+#       #   shop = Shop.all[index]
+#       #   Scraper.scrape_shops[index]
+#       #   # self.scrape_shops
+#       end
+
+#     def display_shop_info(shop)
+#  #
+#     end
+#   end
--- lib/environment.rb
+++ lib/environment.rb
@@ -4,9 +4,3 @@ require_relative "./version"
 require_relative "./cli"
 require_relative "./shop"
 require_relative "./scraper"
-
-# module IceCream
-#   class Error < StandardError; end
-#   # Your code goes here...
-# end
-
