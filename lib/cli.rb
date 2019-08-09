7e45c12b31fadcc8289fce57c4027000c5d3f997
alicia <afasciocco@gmail.com>
2019-08-09 15:18:27 -0400

    now everything is working

--- lib/cli.rb
+++ lib/cli.rb
@@ -14,76 +14,21 @@ class CLI
     def main_menu
       puts "Please select a shop to learn more."
       input = gets.chomp
-      while input != "exit"
+      if input == "exit"
+        exit
 
+      else
       shop = Shop.all[input.to_i - 1]
+
+      end
 
       if shop 
         Scraper.scrape_review(shop)
         puts "Here is the review for #{shop.name}"
         puts "#{shop.review}"
+      
       else
-        puts "Invalid. Try again!"
+        puts "Invalid input. Please try again!"
       end
       main_menu
     end
-
-      # html = open("https://www.theinfatuation.com/seattle/guides/ice-cream-seattle-power-rankings")
-     
-
-      # binding.pry
-     
-      # shop_name = name.uniq
-
-        # shop_names << name
-
-        # shop_names.each.with_index(1) do |name, i|
-        #   puts "#{i}. #{name}"
-      # 
-#         self.welcome
-#         loop do
-#             user_input = main_menu
-#             if user_input == 'exit' || user_input.include?('n')
-#               return
-#             else
-#               self.list_shops
-#               self.choose_shop
-#             end
-#           end
-#         end 
-
-
-#     def welcome
-#         
-#     end
-
-#     def main_menu
-#       puts "Are you in the mood for ice cream?"
-#       input = gets.strip.downcase
-#       return input
-#     end
-
-#     def list_shops 
-#       Scraper.scrape_shops
-#     end
-
-#     def choose_shop
-#       puts "Choose an ice cream shop to read more about it."
-#       Scraper.scrape_shops
-  
-#         input = gets.chomp
-
-
-
-#         Scraper.scrape_review(shop)
-#       # list_shops
-#       #   index = gets.strip.to_i - 1
-#       #   shop = Shop.all[index]
-#       #   Scraper.scrape_shops[index]
-#       #   # self.scrape_shops
-#       end
-
-#     def display_shop_info(shop)
-#  #
-#     end
-#   end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,21 +6,25 @@ class Scraper
       doc = Nokogiri::HTML(open("https://www.theinfatuation.com/seattle/guides/ice-cream-seattle-power-rankings"))
       list = doc.search(".spot-block__title-copy")
       
-        name_list = list.search("h3").map{|h3| h3.text.strip}
-          names = name_list.uniq
+        name_list = list.search("h3").map{|h3| h3.text.strip}.uniq
           url_list = list.search("a").map{|s| s.attr("href")}
-          review_url = url_list.select {|link| link.include?("reviews")}
-      names.each.with_index do |name, index| 
-        shop = Shop.new(name, review_url[index])
-        shop.save
-      end
-    end
+          review_url = url_list.select {|link| link.include?("reviews")} 
+      
+          name_list.each.with_index do |name, index| 
+            shop = Shop.new(name, review_url[index])
+            shop.save
+          end
+        end
 
     def self.scrape_review(shop)
       url = shop.url
       html = open(BASE_URL + url)
       doc = Nokogiri::HTML(html)
-      shop.review = doc.search(".post__content__text-block p").text
-    # binding.pry
+      if doc.search(".post__content__text-block p").text != ""
+        shop.review = doc.search(".post__content__text-block p").text 
+        # binding.pry
+      else 
+        shop.review = doc.search(".spot__description p").text
+      end
    end
 end
